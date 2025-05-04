e3a5ca038aea90c5af279b6191582028501213ad
Ilya <isofgh@gmail.com>
2025-05-04 19:05:29 +0300

    feat: add new users Endpoints

--- app/src/main/java/com/ifmo/rmp/data/api/ApiService.kt
+++ app/src/main/java/com/ifmo/rmp/data/api/ApiService.kt
@@ -4,9 +4,15 @@ import com.ifmo.rmp.data.model.ClubCreateRequest
 import com.ifmo.rmp.data.model.ClubCreateResponse
 import com.ifmo.rmp.data.model.ClubInfoResponse
 import com.ifmo.rmp.data.model.ClubsListResponse
+import com.ifmo.rmp.data.model.FriendOptionsRequest
+import com.ifmo.rmp.data.model.FriendOptionsResponse
+import com.ifmo.rmp.data.model.FriendRequestsListResponse
+import com.ifmo.rmp.data.model.IdByUsernameResponse
 import com.ifmo.rmp.data.model.LoginRequest
 import com.ifmo.rmp.data.model.LoginResponse
 import com.ifmo.rmp.data.model.Profile
+import com.ifmo.rmp.data.model.UserDtoResponse
+import com.ifmo.rmp.data.model.UserUpdateRequest
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -17,6 +23,7 @@ interface ApiService {
     @POST("api/v1/auth/login")
     suspend fun login(@Body request: LoginRequest): LoginResponse
 
+    // теперь используем api/v1/users/{userId}, этот запрос потом надо удалить
     @GET("api/v1/users/{userId}/profile")
     suspend fun getUserProfile(@Path("userId") userId: String): Profile
     
@@ -28,4 +35,29 @@ interface ApiService {
     
     @POST("api/v1/clubs/create")
     suspend fun createClub(@Body request: ClubCreateRequest): ClubCreateResponse
-} 
+
+    @GET("/api/v1/users/friends/notifications")
+    suspend fun getFriendsNotifications(): FriendRequestsListResponse
+
+    @POST("api/v1/users/friends/notifications/accept")
+    suspend fun acceptRequest(@Body request: FriendOptionsRequest): FriendOptionsResponse
+
+    @POST("api/v1/users/friends/notifications/deny")
+    suspend fun denyRequest(@Body request: FriendOptionsRequest): FriendOptionsResponse
+
+    @POST("api/v1/users/friends/request")
+    suspend fun addFriend(@Body request: FriendOptionsRequest): FriendOptionsResponse
+
+    @POST("api/v1/users/friends/remove")
+    suspend fun removeFriend(@Body request: FriendOptionsRequest): FriendOptionsResponse
+
+    @GET("api/v1/users/{userId}")
+    suspend fun userData(@Path("userId") userId: String): UserDtoResponse
+
+    @GET("api/v1/users/username/{userName}")
+    suspend fun getIdByUsername(@Path("userName") userName: String): IdByUsernameResponse
+
+    @POST("api/v1/users/goals/update")
+    suspend fun updateUserProfile(@Body request: UserUpdateRequest): FriendOptionsResponse
+
+}
--- app/src/main/java/com/ifmo/rmp/data/api/NetworkModule.kt
+++ app/src/main/java/com/ifmo/rmp/data/api/NetworkModule.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object NetworkModule {
-    private const val BASE_URL = "http://10.0.2.2:8085/"
+    private const val BASE_URL = "127.0.0.1:8082/"
     
     private var apiService: ApiService? = null
     
